579d31a93a1ce5015a59154770e329e521c86c0e
Ayabanana <a.taba085h@gmail.com>
2024-09-24 13:51:27 +0900

    fix:ルーチン画面のデザインの修正

--- test_app/test_app/RoutineTimerView.swift
+++ test_app/test_app/RoutineTimerView.swift
@@ -21,66 +21,67 @@ struct RoutineTimerView: View {
     }
     
     var body: some View {
-        VStack {
-            GroupBox{
-                Text(routine[currentStepIndex].title)
-                    .font(.title)
-                    .fontWeight(.medium)
-                    .foregroundColor(Color(red: 48/255, green: 178/255, blue: 127/255))
-                    .multilineTextAlignment(.center)
-                    .padding(.top)
-                    
-                
-                GroupBox {
-//                    Spacer()
-                    Text(String(format: "%02d:%02d", remainingTime / 60, remainingTime % 60))
-                        .font(.system(size: 64))
+        ScrollView{
+            VStack {
+                GroupBox{
+                    Text(routine[currentStepIndex].title)
+                        .font(.title)
+                        .fontWeight(.medium)
                         .foregroundColor(Color(red: 48/255, green: 178/255, blue: 127/255))
-                        .padding(1)
+                        .multilineTextAlignment(.center)
+                        .padding(.top)
                     
-                    HStack {
-                        Spacer()
-                        Button(action: startTimer) {
-                            Image(systemName: "play.fill")
-                                .foregroundColor(Color(red: 48/255, green: 178/255, blue: 127/255))
-                        }
+                    GroupBox {
+    //                    Spacer()
+                        Text(String(format: "%02d:%02d", remainingTime / 60, remainingTime % 60))
+                            .font(.system(size: 64))
+                            .foregroundColor(Color(red: 48/255, green: 178/255, blue: 127/255))
+                            .padding(1)
                         
-                        ProgressView(value: Double(routine[currentStepIndex].duration * 60 - remainingTime), total: Double(routine[currentStepIndex].duration * 60))
-                            .padding()
-                            .frame(width: 200.0)
-                            .tint(Color(red: 48/255, green: 178/255, blue: 127/255))
-                        
-                        
-                        Button(action: pauseTimer) {
-                            Image(systemName: "pause.fill")
-                                .foregroundColor(Color(red: 48/255, green: 178/255, blue: 127/255))
+                        HStack {
+                            Spacer()
+                            Button(action: startTimer) {
+                                Image(systemName: "play.fill")
+                                    .foregroundColor(Color(red: 48/255, green: 178/255, blue: 127/255))
+                            }
                             
+                            ProgressView(value: Double(routine[currentStepIndex].duration * 60 - remainingTime), total: Double(routine[currentStepIndex].duration * 60))
+                                .padding()
+                                .frame(width: 200.0)
+                                .tint(Color(red: 48/255, green: 178/255, blue: 127/255))
+                            
+                            
+                            Button(action: pauseTimer) {
+                                Image(systemName: "pause.fill")
+                                    .foregroundColor(Color(red: 48/255, green: 178/255, blue: 127/255))
+                                
+                            }
+                            Spacer()
                         }
-                        Spacer()
                     }
+                    .padding(.bottom)
+    //                .frame(width: nil, height: 200.0)
+                    .backgroundStyle(Color(red: 238/255, green: 240/255, blue: 237/255))
                 }
-                .padding(.bottom)
-//                .frame(width: nil, height: 200.0)
                 .backgroundStyle(Color(red: 238/255, green: 240/255, blue: 237/255))
-            }
-            .backgroundStyle(Color(red: 238/255, green: 240/255, blue: 237/255))
-            .padding()
-            
-            
-            GroupBox{
-                ForEach(0..<routine.count, id: \.self) { index in
-                    HStack {
-                        Text(routine[index].title)
-                        Spacer()
-                        Text("\(routine[index].duration)分")
+                .padding()
+                
+                
+                GroupBox{
+                    ForEach(0..<routine.count, id: \.self) { index in
+                        HStack {
+                            Text(routine[index].title)
+                            Spacer()
+                            Text("\(routine[index].duration)分")
+                        }
+                        .padding()
+                        .background(index < currentStepIndex ? Color(red: 200/255, green: 223/255, blue: 214/255) : (index == currentStepIndex ? Color.yellow.opacity(0.5) : Color.white))
+                        .cornerRadius(8)
                     }
-                    .padding()
-                    .background(index < currentStepIndex ? Color(red: 200/255, green: 223/255, blue: 214/255) : (index == currentStepIndex ? Color.yellow.opacity(0.5) : Color.white))
-                    .cornerRadius(8)
                 }
-            }
-            .backgroundStyle(Color(red: 238/255, green: 240/255, blue: 237/255))
-            .padding()
+                .backgroundStyle(Color(red: 238/255, green: 240/255, blue: 237/255))
+                .padding()
+        }
         }
         .onDisappear {
             timer?.invalidate()
@@ -136,3 +137,4 @@ struct RoutineTimerView: View {
         Routine(title: "着替える", duration: 5)
     ])
 }
+
