b3257e804d8cfe1d4300afba3978874dd60a50f7
Ayabanana <a.taba085h@gmail.com>
2024-12-09 13:47:54 +0900

    loginとsignup機能の実装をする

--- test_app/test_app/UserModel.swift
+++ test_app/test_app/UserModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class UserModel: ObservableObject {
+    @Published var id: Int = 0
+    @Published var name: String = ""
+    @Published var email: String = ""
+    
+    func update(from dictionary: [String: Any]) {
+        self.id = dictionary["id"] as? Int ?? 0
+        self.name = dictionary["name"] as? String ?? ""
+        self.email = dictionary["email"] as? String ?? ""
+    }
+}
+
